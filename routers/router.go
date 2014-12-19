b72a2fb8f0931ea693385349dca62ee4e695c29b
tolower <lower_scoring@126.com>
2014-12-20 01:12:47 +0800

    1

--- routers/router.go
+++ routers/router.go
@@ -6,6 +6,11 @@ import (
 )
 
 func init() {
+	//设置静态目录
+	beego.SetStaticPath("/img", "static/img")
+	beego.SetStaticPath("/css", "static/css")
+	beego.SetStaticPath("/js", "static/js")
+	//设置路由规则
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/login", &controllers.LoginController{})
 }
